91e1c41cc455caa884c4a1a0e1d4b2879b263508
Josh Taylor <joshrtay@gmail.com>
2016-08-14 21:47:54 -0700

    test go, join, select, kill, delay

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@
 require("babel-polyfill")
 
 const test = require('tape')
-const {middleware, go, join, poss} = require('..')
+const {dispatch, go, join, poss, delay, select, kill} = require('..')
 const bind = require('@f/bind-middleware')
 const sleep = require('@f/sleep')
 
@@ -15,19 +15,71 @@ const sleep = require('@f/sleep')
 
 test('go should run async and return', (t) => {
 
-  let dispatch = bind([middleware()])
-
   dispatch(function * () {
-    var p = yield go(function * () {
-      yield sleep(10)
+    const p = yield go(function * () {
+      yield delay(10)
       return 'foo'
     })
 
     const [err, res] = yield poss(join(p))
     t.equal(res, 'foo')
     t.end()
-
   })
 
+})
 
+test('join should wait on multiple', (t) => {
+  dispatch(function * () {
+    const p1 = yield go(function * () {
+      yield delay(10)
+      return 'foo'
+    })
+    const p2 = yield go(function * () {
+      yield delay(20)
+      return 'bar'
+    })
+
+    const [err, res] = yield poss(join([p1, p2]))
+    t.deepEqual(res, ['foo', 'bar'])
+    t.end()
+  })
+})
+
+test('join should selct fastest', (t) => {
+  dispatch(function * () {
+    const p1 = yield go(function * () {
+      yield delay(20)
+    })
+    const p2 = yield go(function * () {
+      yield delay(10)
+    })
+
+    const [err, res] = yield poss(select([
+      [p1, _ => 'foo'],
+      [p2, _ => 'bar']
+    ]))
+
+    t.deepEqual(res, 'bar')
+    t.end()
+  })
+})
+
+test('should kill ongoing gos', (t) => {
+  dispatch(function * () {
+    const ended = []
+    const p1 = yield go(function * () {
+      yield delay(200)
+      ended.push(true)
+    })
+    const p2 = yield go(function * () {
+      yield delay(200)
+      ended.push(true)
+    })
+
+    const [err, res] = yield poss(kill())
+    t.equal(err.message, 'STOP_EXECUTION')
+    yield delay(200)
+    t.equal(ended.length, 0)
+    t.end()
+  })
 })
